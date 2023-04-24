ca32d90b11f49e207dfa81dcb33831bd5885ed7e
Khaled Mihoub <49345013+khaledmihoub@users.noreply.github.com>
2023-04-24 14:25:10 +0100

    khaled

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,26 @@ import { Injectable } from '@angular/core';
 })
 export class UserService {
 
-  constructor() { }
+  baseUrl="http://127.0.0.1:8085"
+  constructor(private http:HttpClient) { }
+  getusers(token)
+  {
+
+
+    return this.http.get<any>(this.baseUrl+"/user/retrieve-users",
+        {
+          headers:new HttpHeaders({ authorization : 'Bearer '+token })
+        })
+  }
+
+  deleteuser(data, token )
+  {
+
+
+    return this.http.get<any>(this.baseUrl+"/user/deleteuser/"+data, {
+      headers:new HttpHeaders({ authorization : 'Bearer '+token })
+    })
+  }
+
+
 }
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookiesService } from '../services/cookie/cookies.service';
+import { UserService } from '../services/user.service';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private us: UserService, private cs:CookiesService ) {
+    //get token in constructeur and pass it to APIs
+this.jwt=this.cs.getCookieJWT().toString();
+this.id=this.cs.getCookieIDUser();
+ //console.log(this.jwt);
+
+}
+id : number;
+jwt : string;
+users: any[];
+addform = new FormGroup({
+
+  title: new FormControl('', []),
+  detail: new FormControl('', []),
+  goalAmount: new FormControl('', []),
+  expireDate: new FormControl('', []),
+
+})
 
   ngOnInit(): void {
+    this.us.getusers(this.jwt).subscribe(res=>{
+      console.log(res);
+      this.users=res;
+    })
   }
+  adduser(){
 
+  }
+  delete(idp){
+    this.us.deleteuser(idp,this.jwt).subscribe(res=>{
+      console.log(res);
+      window.location.reload();
+  })
+  }
 }
